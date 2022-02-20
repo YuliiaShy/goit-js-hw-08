ed0bc7c7f7d60429b41f201b16d1022a74de5d6e
Yuliia Shybyk <j7557500@gmail.com>
2022-02-20 21:41:39 +0200

    task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+import throttle from 'lodash.throttle';
+
+import Player from '@vimeo/player';
+ 
+const player = new Player('vimeo-player');
+
+
+
+const onTimeUpdate = function (data) {
+    localStorage.setItem('videoplayer-current-time', data.seconds);
+};
+
+player.on('timeupdate', throttle(onTimeUpdate, 1000));
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+    
+
+   
+
 

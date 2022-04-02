3b2ff57efcd8438c60fb3439fadbad06b73f7d1f
Yuliia Shybyk <j7557500@gmail.com>
2022-04-02 15:49:47 +0300

    task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -7,22 +7,10 @@ const STORAGE_KEY = 'feedback-form-state';
 
 const dataForm = {};
 
-formEl.addEventListener('submit', onSubmitForm);
-formEl.addEventListener('input', throttle(onInputForm, 500));
-
 initForm();
 
-function initForm() {
-  const localStorageGet = localStorage.getItem(STORAGE_KEY);
-
-  if (localStorageGet) {
-    const { email, message } = JSON.parse(localStorageGet);
-    formEl.email.value = email;
-    formEl.message.value = message;
-    dataForm.email = email;
-    dataForm.message = message;
-  }
-}
+formEl.addEventListener('submit', onSubmitForm);
+formEl.addEventListener('input', throttle(onInputForm, 500));
 
 function onInputForm(event) {
   dataForm[event.target.name] = event.target.value;
@@ -30,12 +18,38 @@ function onInputForm(event) {
   localStorage.setItem(STORAGE_KEY, JSON.stringify(dataForm));
 }
 
+function initForm() {
+  const localStorageGet = localStorage.getItem(STORAGE_KEY);
+
+  if (localStorageGet) {
+    const { email, message } = JSON.parse(localStorageGet);
+    formEl.email.value = email || '';
+    formEl.message.value = message || '';
+    dataForm.email = email;
+    dataForm.message = message;
+  }
+}
+
 function onSubmitForm(event) {
   event.preventDefault();
+
+  const {
+    elements: { email, message },
+  } = event.currentTarget;
+
+  if (email.value === '' || message.value === '') {
+    alert('Please fill in all the fields!');
+    return;
+  }
+
+  const formData = new FormData(event.currentTarget);
+
+  formData.forEach((value, key) => {
+    console.log(`${key}, ${value}`);
+  });
 
   formEl.reset();
 
   localStorage.removeItem(STORAGE_KEY);
-
-  console.log(dataForm);
 }
+
